4ccc97637f58127ca111a03143bab9b9e9f6485c
anibalmunoz <munoz4hernandez@gmail.com>
2022-05-16 22:38:55 -0600

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConnectionPageComponent } from './connection-page/connection-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'connection', component:ConnectionPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
