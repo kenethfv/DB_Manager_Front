f1bf6c5fc3f6cb18b5e92f41cd57fe969356b090
anibalmunoz <munoz4hernandez@gmail.com>
2022-05-16 23:57:35 -0600

    Inicio de creación de pagina HOME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+//import { TableModule } from 'primeng/table';
+// import { ToastModule } from 'primeng/toast';
+// import { MenubarModule } from 'primeng/menubar';
+// import { TabMenuModule } from 'primeng/tabmenu';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { DropdownModule } from 'primeng/dropdown';
+// import { ConfirmDialogModule } from 'primeng/confirmdialog';
+// import { DynamicDialogModule } from 'primeng/dynamicdialog';
+// import { MessagesModule } from 'primeng/messages';
+// import { DialogModule } from 'primeng/dialog';
+
 import { HomeComponent } from './home/home.component';
 import { ConnectionPageComponent } from './connection-page/connection-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ConnectionPageComponent
-  ],
+  declarations: [AppComponent, HomeComponent, ConnectionPageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule,
+     //TableModule,
+    // ToastModule,
+    // MenubarModule,
+    // TabMenuModule,
+    // BrowserAnimationsModule,
+   // DropdownModule,
+    // ConfirmDialogModule,
+    // DynamicDialogModule,
+    // MessagesModule,
+    // DialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
