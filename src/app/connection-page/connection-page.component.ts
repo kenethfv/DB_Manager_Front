47763f7ffc1b49ff558066f667ff33e3d4d56ab5
anibalmunoz <munoz4hernandez@gmail.com>
2022-05-24 00:32:17 -0600

    consumo de servicio get

--- src/app/connection-page/connection-page.component.ts
+++ src/app/connection-page/connection-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ConnectionService } from '../services/connection_service/connection.service';
 
 @Component({
   selector: 'app-connection-page',
@@ -8,10 +9,37 @@ import { DynamicDialogRef } from 'primeng/dynamicdialog';
 })
 export class ConnectionPageComponent implements OnInit {
   formProperties: any = {};
+  formLogin: any = {};
 
-  constructor(public ref: DynamicDialogRef) {}
+  constructor(
+    public ref: DynamicDialogRef,
+    private connectionService: ConnectionService
+  ) {}
 
   ngOnInit(): void {}
 
-  enviarFormulario() {}
+  enviarFormulario() {
+    let formulario: any = document.getElementById('crearConexion');
+
+    this.formLogin.host = this.formProperties.host;
+    this.formLogin.user = this.formProperties.user;
+    this.formLogin.password = this.formProperties.password;
+
+
+    console.log(this.formLogin);
+
+    this.connectionService
+      .login(this.formLogin)
+      .subscribe((res: any) => this.finalizarGuardar(res));
+    //this.mostrarEditarToast();
+    formulario.reset();
+    this.formProperties = {};
+    // this.mostrarFormulario = false;
+    // this.reset();
+    // this.mostrarBotonNuevo = true;
+  }
+
+  finalizarGuardar(respuesta: any) {
+    console.log(respuesta);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,46 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ConnectionPageComponent } from '../connection-page/connection-page.component';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { DialogService } from 'primeng/dynamicdialog';
+import { ConnectionService } from '../services/connection_service/connection.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers:[DialogService],
+  providers: [DialogService],
 })
 export class HomeComponent implements OnInit {
-
   ref?: DynamicDialogRef;
 
-  lang = [
-    { name: 5 },
-    { name: 10 },
-    { name: 15 },
-  ];
+  lang = [{ name: 5 }, { name: 10 }, { name: 15 }];
 
-  conexiones = [
-    {
-      esquema:"HR",
-      usuario:"RRHH",
-    },
-    {
-      esquema:"UMG",
-      usuario:"RMaldonado",
-    }
-  ];
+  conexiones = [];
 
- primeraPagina = false;
+  primeraPagina = false;
 
-  constructor(public dialogService: DialogService) { }
+  constructor(
+    public dialogService: DialogService,
+    private connectionService: ConnectionService
+  ) {}
 
   ngOnInit(): void {
+    this.obtenerConexiones();
   }
 
-  formularioComponent(){
+  formularioComponent() {
     this.ref = this.dialogService.open(ConnectionPageComponent, {
       header: 'Nueva conexión',
       width: '70%',
-      contentStyle: { "max-height": "500px", "overflow": "auto" },
+      contentStyle: { 'max-height': '500px', overflow: 'auto' },
       baseZIndex: 10000,
     });
 
@@ -54,4 +45,14 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  obtenerConexiones() {
+    this.connectionService
+      .getConnections()
+      .subscribe((res: any) => this.mostrarClientes(res));
+  }
+
+  mostrarClientes(clients: any) {
+    console.log(clients.data);
+    this.conexiones = clients.data;
+  }
 }
