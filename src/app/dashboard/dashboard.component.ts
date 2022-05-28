6dd6148a9c07f05c75497c29ea9ad452a16ce947
anibalmunoz <munoz4hernandez@gmail.com>
2022-05-27 19:08:15 -0600

    usuario en localstorage

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { BindingScope } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../services/dashboard_services/dashboard.service';
 
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -18,12 +17,12 @@ export class DashboardComponent implements OnInit {
   static selectableTextArea: NodeListOf<Element>;
   //selectedText: String = "";
 
-  
+  //objeto de local storage
+  usuarioConectado: any = {};
 
   constructor(private dashboardService: DashboardService) {}
 
   ngOnInit(): void {
-    
     //this.llenarTablas(this.tablas);
     this.llamarMetodo();
     DashboardComponent.selectableTextArea =
@@ -39,12 +38,10 @@ export class DashboardComponent implements OnInit {
         document.getElementById('resultado')
       )).value = selectedText || '');
     }
+
+    //
+    this.obtener_localStorage();
   }
-
- 
-  
-
-
 
   limpiarEditor() {
     let limpiaTexto = ((<HTMLInputElement>(
@@ -53,8 +50,8 @@ export class DashboardComponent implements OnInit {
   }
 
   limpiarResultado() {
-    let claseError = document.querySelector('.error')
-    let claseSuccess = document.querySelector('.success')
+    let claseError = document.querySelector('.error');
+    let claseSuccess = document.querySelector('.success');
     const div = document.querySelector('#resultado');
     claseError?.remove();
     claseSuccess?.remove();
@@ -62,12 +59,11 @@ export class DashboardComponent implements OnInit {
 
   mostrarResultado() {
     const selectedText = window.getSelection()?.toString().trim();
-    console.log("texto: "+selectedText)
+    console.log('texto: ' + selectedText);
     const regSelect = /select [a-zA-Z1-9*=<>+^(),.%/ ]+\;/gi;
     const regDelete = /delete [a-zA-Z1-9*=<>+^(),.%/ ]+\;/gi;
 
-    
-    let queryObject:any = {};
+    let queryObject: any = {};
     queryObject.host = this.host;
     queryObject.user = 'admin';
     queryObject.password = 'd2cany8bdwypjtACDvaq';
@@ -77,17 +73,16 @@ export class DashboardComponent implements OnInit {
     if (selectedText?.match(regSelect)) {
       //alert('running select script');
       this.dashboardService
-      .dashboardQuery(queryObject)
-      .subscribe((res: any) => this.respuestaQuery(res));
-    } else /*if (selectedText?.match(regDelete))*/ {
+        .dashboardQuery(queryObject)
+        .subscribe((res: any) => this.respuestaQuery(res));
+    } /*if (selectedText?.match(regDelete))*/ else {
       //alert('running delete script');
       this.dashboardService
-      .dashboardQuery(queryObject)
-      .subscribe((res: any) => this.resQuery(res));
-    } 
-    
+        .dashboardQuery(queryObject)
+        .subscribe((res: any) => this.resQuery(res));
+    }
 
-      /*let limpiaTexto = 'Answer from service will go here ';
+    /*let limpiaTexto = 'Answer from service will go here ';
       let agrega = ((<HTMLInputElement>document.getElementById('result')).value =
       limpiaTexto);*/
   }
@@ -99,63 +94,61 @@ export class DashboardComponent implements OnInit {
     this.queryInfo(this.data);
   }
 
-  queryInfo(info:any){
+  queryInfo(info: any) {
     console.log(info);
     const div = document.querySelector('#resultado');
     const tb = document.createElement('table');
-    tb.className = "success"
+    tb.className = 'success';
     const tr = document.createElement('tr');
-    let objectProp = Object.keys(info[0])
+    let objectProp = Object.keys(info[0]);
 
-    objectProp.forEach((pr:any) => {
+    objectProp.forEach((pr: any) => {
       let header = document.createElement('th');
-      let headerText = document.createTextNode(pr)
-      header.appendChild(headerText)
-      tr.appendChild(header)
-             
+      let headerText = document.createTextNode(pr);
+      header.appendChild(headerText);
+      tr.appendChild(header);
     });
     tb.appendChild(tr);
-  
-    info.forEach((arr:any, index:number) => {   
-      let objectValue = Object.values(info[index])  
+
+    info.forEach((arr: any, index: number) => {
+      let objectValue = Object.values(info[index]);
       const tr2 = document.createElement('tr');
-      objectValue.forEach((val:any) => {
+      objectValue.forEach((val: any) => {
         let header = document.createElement('td');
-        let headerText = document.createTextNode(val)
-        header.appendChild(headerText)
-        tr2.appendChild(header)         
+        let headerText = document.createTextNode(val);
+        header.appendChild(headerText);
+        tr2.appendChild(header);
       });
       tb.appendChild(tr2);
-    
-    });    
+    });
     div?.appendChild(tb);
   }
 
   resQuery(respuesta: any) {
     this.response = respuesta;
     this.data = this.response.data;
-    
+
     console.log(this.response);
-    this.queryInfo2(this.response)
-    
+    this.queryInfo2(this.response);
   }
 
-  queryInfo2(info:any){
-    console.log(info)
+  queryInfo2(info: any) {
+    console.log(info);
     const status = info.statusCode;
     const data = info.data;
     const div = document.querySelector('#resultado');
     const p = document.createElement('p');
-    let statusText = document.createTextNode(`Operacion exitosa => StatusCode: ${status}`)
-    p.appendChild(statusText)
-    p.className = "success";
-    for(let val in data){
-      let text = document.createTextNode(`${val}: ${data[val]}`)
-      p.appendChild(text)
+    let statusText = document.createTextNode(
+      `Operacion exitosa => StatusCode: ${status}`
+    );
+    p.appendChild(statusText);
+    p.className = 'success';
+    for (let val in data) {
+      let text = document.createTextNode(`${val}: ${data[val]}`);
+      p.appendChild(text);
     }
     div?.appendChild(p);
   }
-
 
   llamarMetodo() {
     this.objeto.host = this.host;
@@ -168,33 +161,37 @@ export class DashboardComponent implements OnInit {
       .subscribe((res: any) => this.finalizarGuardar(res));
   }
 
-  
-
   finalizarGuardar(respuesta: any) {
     this.response = respuesta;
     this.data = this.response.data;
     console.log(this.response);
     console.log(this.data);
-    this.llenarTablas(this.data)
+    this.llenarTablas(this.data);
   }
 
-  llenarTablas(tb:any){
-    let claseTabla = document.querySelector('.lista-tablas')
+  llenarTablas(tb: any) {
+    let claseTabla = document.querySelector('.lista-tablas');
     claseTabla?.remove();
     const div = document.querySelector('#aside');
     const lista = document.createElement('ul');
-    lista.className = "lista-tablas";
-    tb.forEach((object:any) => {
-      for(let value in object){
-        console.log(object[value])
+    lista.className = 'lista-tablas';
+    tb.forEach((object: any) => {
+      for (let value in object) {
+        console.log(object[value]);
         let line = document.createElement('li');
-        let textLine = document.createTextNode(object[value])
-        line.appendChild(textLine)
-        lista.appendChild(line)
-      }       
-  });
+        let textLine = document.createTextNode(object[value]);
+        line.appendChild(textLine);
+        lista.appendChild(line);
+      }
+    });
 
-  div?.appendChild(lista);
+    div?.appendChild(lista);
   }
 
+  //OBTENER INFO DE USUARIO CONECTADO DE
+  obtener_localStorage() {
+    this.usuarioConectado = JSON.parse(localStorage.getItem('dataConection')!);
+    console.log("EL USUARIO LOGUEADO ES:");
+    console.log(this.usuarioConectado);
+  }
 }
